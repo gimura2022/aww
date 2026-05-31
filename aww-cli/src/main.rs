c21740942de0b01660dc5a3f35b81a1379a84b6a
gimura <gimura0001@gmail.com>
2026-05-31 23:54:56 +0300

    aww-cli: make --config and --executable optional

--- aww-cli/src/main.rs
+++ aww-cli/src/main.rs
@@ -9,25 +9,32 @@ use miette::miette;
 enum Args {
     Spawn {
         #[arg(short = 'c', long)]
-        config: PathBuf,
+        config: Option<PathBuf>,
 
-        #[arg(short = 'e', long)]
+        #[arg(short = 'e', long, default_value = "aww-daemon")]
         executable: String
     },
     Refresh {
         #[arg(short = 'c', long)]
-        config: PathBuf
+        config: Option<PathBuf>
     },
     Kill
 }
 
-fn is_daemon_running() -> miette::Result<bool> {
+fn daemon_running() -> miette::Result<bool> {
     Ok(BaseDirs::new()
         .ok_or(miette!("can't get runtime dir"))?
         .runtime_dir()
         .ok_or(miette!("can't get runtime dir"))?
         .join("aww-ipc")
         .exists())
+}
+
+fn config_path() -> miette::Result<PathBuf> {
+    Ok(BaseDirs::new()
+        .ok_or(miette!("can't get config dir"))?
+        .preference_dir()
+        .join("aww"))
 }
 
 fn spawn(_config: PathBuf, _executable: String) -> miette::Result<()> {
@@ -45,13 +52,13 @@ fn kill() -> miette::Result<()> {
 fn main() -> miette::Result<()> {
     match Args::parse() {
         Args::Spawn { config, executable } => {
-            if is_daemon_running()? {
-                refesh(config)
+            if daemon_running()? {
+                refesh(config.unwrap_or(config_path()?))
             } else {
-                spawn(config, executable)
+                spawn(config.unwrap_or(config_path()?), executable)
             }
         }
-        Args::Refresh { config } => refesh(config),
+        Args::Refresh { config } => refesh(config.unwrap_or(config_path()?)),
         Args::Kill => kill()
     }
 }
