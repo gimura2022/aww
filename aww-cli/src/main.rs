e4fb08db42b1693c3dfcd60b6d5ddbc39ffa76fe
gimura <gimura0001@gmail.com>
2026-05-31 23:44:13 +0300

    aww-cli: added new subcommand and rewrited logic

--- aww-cli/src/main.rs
+++ aww-cli/src/main.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use directories::BaseDirs;
+use miette::miette;
 
 #[derive(Parser)]
 #[clap(version)]
@@ -10,17 +12,22 @@ enum Args {
         config: PathBuf,
 
         #[arg(short = 'e', long)]
-        executable: String,
+        executable: String
     },
-    Refesh {
+    Refresh {
         #[arg(short = 'c', long)]
-        config: PathBuf,
+        config: PathBuf
     },
-    Kill,
+    Kill
 }
 
-fn is_daemon_running() -> bool {
-    todo!()
+fn is_daemon_running() -> miette::Result<bool> {
+    Ok(BaseDirs::new()
+        .ok_or(miette!("can't get runtime dir"))?
+        .runtime_dir()
+        .ok_or(miette!("can't get runtime dir"))?
+        .join("aww-ipc")
+        .exists())
 }
 
 fn spawn(_config: PathBuf, _executable: String) -> miette::Result<()> {
@@ -37,10 +44,14 @@ fn kill() -> miette::Result<()> {
 
 fn main() -> miette::Result<()> {
     match Args::parse() {
-        Args::Spawn { config, executable } => if is_daemon_running() { refesh(config) } else { spawn(config, executable) },
-        Args::Refesh { config } => refesh(config),
-        Args::Kill => kill(),
-    }?;
-
-    Ok(())
+        Args::Spawn { config, executable } => {
+            if is_daemon_running()? {
+                refesh(config)
+            } else {
+                spawn(config, executable)
+            }
+        }
+        Args::Refresh { config } => refesh(config),
+        Args::Kill => kill()
+    }
 }
