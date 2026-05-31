81dfd5c6e0be4704fd6ffe777ab6cf0109d81e27
gimura <gimura0001@gmail.com>
2026-05-31 23:01:36 +0300

    added basic cli code

--- aww-cli/src/main.rs
+++ aww-cli/src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version)]
+enum Args {
+    Spawn {
+        #[arg(short = 'c', long)]
+        config: PathBuf,
+    },
+    Kill,
+}
+
+fn spawn(_config: PathBuf) -> miette::Result<()> {
+    todo!()
+}
+
+fn kill() -> miette::Result<()> {
+    todo!()
+}
+
+fn main() -> miette::Result<()> {
+    match Args::parse() {
+        Args::Spawn { config } => spawn(config),
+        Args::Kill => kill(),
+    }?;
+
+    Ok(())
+}
